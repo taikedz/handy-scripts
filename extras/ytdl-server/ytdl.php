bbddf4a1b8e4ffdb1a7856e0e190470d944baf81
Tai Kedzierski <dch.tai@gmail.com>
2015-02-05 18:02:58 +0000

    YTDL PHP en route

--- extras/ytdl-server/ytdl.php
+++ extras/ytdl-server/ytdl.php
@@ -11,17 +11,37 @@ These get written to a working file after sanitizing
 A script gets kicked off to pass these to youtube-dl
 */
 
+$YTDLBIN='/home/tai/bin/youtube-dl';
+
+
+if ( array_key_exists('yturls',$_POST) ) {
+	$allurls = explode('\n|\r|\n\r|\s',$_POST['yturls']);
+	foreach( $allurls as $yturl ) {
+		preg_match('/(\?|&)v=([a-zA-Z0-9_-]+)&?/',$yturl,$regres);
+		$ytvid=$regres[2]; 
+		if($ytvid==''){continue;}
+		# print( "<p>[ $ytvid FROM $yturl ]</p>" );
+		print("bash -c \"$YTDLBIN -x --audio-format vorbis 'http://youtube.com/watch?v=$ytvid'\"");#,$ytdloutput);
+		print_r($ytdloutput);
+		exec("ls | grep '$ytvid'",$fileres);
+		print_r($fileres);
+		#$resfile=$fileres[0];
+		#$newresfile=preg_replace('/[^a-zA-Z0-9_.-]+/','_',$resfile);
+		#exec("mv '$resfile' files/$newresfile");
+		?><p><a href="<?= "files/$newresfile" ?>">Download <?= $resfile ?></a></p><?php
+	}
+}
 
 ?>
 <html>
-<head> <title>YouTube Downloader</title>
+<head><title>YouTube Downloader</title>
 </head>
 <body>
 
 <form method="POST" action="ytdl.php">
 
 <p id="url_input">
-<label for="yturls" id="url_label">YouTube URLs</label><br />
+<label for="yturls" id="url_label">YouTube URLs (one per line)</label><br />
 <textarea id="yturls" name="yturls"></textarea>
 </p>
 
