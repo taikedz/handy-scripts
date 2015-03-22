4fa061e97dc190a80f3f14f49cf684e71aaa3d80
Tai Kedzierski @ PXE <dch.tai@gmail.com>
2015-03-22 18:31:53 +0000

    ISO customization based on instructions at https://help.ubuntu.com/community/InstallCDCustomization

---
+++ in-progress/customiso/mkiso.sh
@@ -0,0 +1,9 @@
+IMAGE=custom.iso
+BUILD=/opt/cd-image/
+
+mkisofs -r -V "Custom Ubuntu Install CD" \
+            -cache-inodes \
+            -J -l -b isolinux/isolinux.bin \
+            -c isolinux/boot.cat -no-emul-boot \
+            -boot-load-size 4 -boot-info-table \
+            -o $IMAGE $BUILD
