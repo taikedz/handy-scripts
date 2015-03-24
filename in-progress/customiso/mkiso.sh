3ec50d6cc57349036018084436edea7be37d86b0
Tai Kedzierski <dch.tai@gmail.com>
2015-03-24 10:46:44 +0000

    Moved customiso to its own repo

--- in-progress/customiso/mkiso.sh
+++
@@ -1,9 +0,0 @@
-IMAGE=custom.iso
-BUILD=/opt/cd-image/
-
-mkisofs -r -V "Custom Ubuntu Install CD" \
-            -cache-inodes \
-            -J -l -b isolinux/isolinux.bin \
-            -c isolinux/boot.cat -no-emul-boot \
-            -boot-load-size 4 -boot-info-table \
-            -o $IMAGE $BUILD
