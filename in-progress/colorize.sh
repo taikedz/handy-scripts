8ddcf20a151d8b8be4783efcdc201e43dd303473
Tai Kedzierski <dch.tai@gmail.com>
2015-06-22 13:39:42 +0100

    Colorization notes

--- in-progress/colorize.sh
+++ in-progress/colorize.sh
@@ -1,6 +1,6 @@
 #To colorize, you need to use echo with escapes
 
-echo          ' Colour ID        echo "\033[0;$i"mlight text           echo "\033[1;$i"mbold text'
+echo          ' Colour ID         echo \033[0;"$i"mlight text           echo \033[1;"$i"mbold text'
 
 for i in {3,4} {30..47}; do
 cat << EOF
