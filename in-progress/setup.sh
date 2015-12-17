d63efe514a2e0f43687e51d17945c64f7a89833d
Tai @ Flex <dch.tai@gmail.com>
2015-12-17 11:27:03 +0000

    Added PS1 fanciness, corrected vimrc

--- in-progress/setup.sh
+++ in-progress/setup.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-if [[ $UID != 0 ]]; then
-	echo "You must be root"
-	exit 1
+if [[ $UID = 0 ]]; then
+	if [[ $(uname -a | grep -i ubuntu -c) -gt 0 ]]; then
+		apt-get update && apt-get upgrade
+		apt-get install htop zenity gksu
+	fi
 fi
-
-# This is an example initial setup script for Ubuntu
-
-apt update && apt upgrade
-apt install htop zenity gksu
 
 cat <<EOF >> $HOME/.bashrc
 # ==== Automatic customization
-
-VISUAL=/usr/bin/vim; export VISUAL
-EDITOR=\$VISUAL; export EDITOR
+export VISUAL=/usr/bin/vim
+export EDITOR=\$VISUAL
 alias crontab='crontab -i'
 alias ll='ls -l'
+export PS1="\[\033[01;37m\]\$? \$(if [[ \$? == 0 ]]; then echo \"\[\033[01;32m\]\342\234\223\"; else echo \"\[\033[01;31m\]\342\234\227\"; fi) $(if [[ ${EUID} == 0 ]]; then echo '\[\033[01;31m\]\h'; else echo '\[\033[01;32m\]\u@\h'; fi)\[\033[01;34m\] \w \$\[\033[00m\] "
 # =====/
 EOF
 
 cat <<EOF >> $HOME/.vimrc
-# ==== Automatic customization
+" ==== Automatic customization
 syntax on
 set autoindent
 set formatoptions=tcqr
 colorscheme desert
 set hlsearch
-
-# ====== /
+" ====== /
+EOF
