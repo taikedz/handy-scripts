c2cddb1dc74edc99b367f12f2b8065e12a581e69
Tai <dch tai gmail com>
2015-12-28 14:09:37 +0000

    Create users etc too

--- modules/opconn/bin/lib/create-db.sh
+++ modules/opconn/bin/lib/create-db.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/mysql
 
+create database stragglers;
+create user 'straggler'@'localhost' identified by 'raggle';
+grant all privileges on stragglers.* to 'straggler'@'localhost';
+
+use stragglers;
 create table Connections (
     id char(32) primary key,
     Content varchar(128),
