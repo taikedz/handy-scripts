4337d1c49f822674bac977f6b0ede005d5119579
Tai @ Flex <dch.tai@gmail.com>
2015-12-28 13:41:33 +0000

    Initial tracking of past connections

--- modules/opconn/bin/lib/straggle-lib.sh
+++ modules/opconn/bin/lib/straggle-lib.sh
@@ -1,10 +1,56 @@
 #!/bin/bash
 
-function insert {
+function stg_insert {
 portserv="$1"
+psmd5=$( stg_md5 "$portserv" )
 
 cat <<EOSQL | mysql -u "$DBUSER" -p"$DBPASS" "$DBNAME"
-insert into Connections(id,content) values (MD5("$portserv"),"$(date "+%F-%T")");
+insert into Connections(id,content) values ("$psmd5","$(date "+%F-%T")");
 EOSQL
 }
+
+function stg_md5 {
+echo -n "$1"|md5sum|sed -r 's/\s*-\s*$//'
+}
+
+function stg_get {
+	cat <<EOSQL|mysql -u "$DBUSER" -p"$DBPASS" "$DBNAME"
+select * from Connections where id='$1';
+EOSQL
+}
+function stg_getcontent {
+	cat <<EOSQL|mysql -N -u "$DBUSER" -p"$DBPASS" "$DBNAME"
+select Content from Connections where id='$1';
+EOSQL
+}
+function stg_getnotes {
+	cat <<EOSQL|mysql -N -u "$DBUSER" -p"$DBPASS" "$DBNAME"
+select Notes from Connections where id='$1';
+EOSQL
+}
+
+function stg_rm {
+	cat <<EOSQL|mysql -u "$DBUSER" -p"$DBPASS" "$DBNAME"
+delete from Connections where id='$1' limit 1;
+EOSQL
+}
+
+function stg_setcontent {
+	md5id=$1
+	shift
+	newcontent=$@
+	cat <<EOSQL|mysql -u "$DBUSER" -p"$DBPASS" "$DBNAME"
+update Connections where id='$md5id' set Content='$newcontent' limit 1;
+EOSQL
+}
+
+function stg_setnotes {
+	md5id=$1
+	shift
+	newcontent=$@
+	cat <<EOSQL|mysql -u "$DBUSER" -p"$DBPASS" "$DBNAME"
+update Connections where id='$md5id' set Notes='$newcontent' limit 1;
+EOSQL
+}
+
 
