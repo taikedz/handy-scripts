6660a66f56ee6f8668269f3e8d127ffba54b72c1
Tai @ Flex <dch.tai@gmail.com>
2016-04-20 14:39:08 +0100

    Add checks and install option

--- projects/gdiff/buildme.sh
+++ projects/gdiff/buildme.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-bbuild src/gdiff
-mkdir -p test/
-mv gdiff.tgz test/
+if [[ -z "$*" ]]; then
+	echo "Please specify a file to compile"
+	exit 1
+fi
 
-echo "Move output to test/"
+bfile="$1"
+bname="$(basename "$bfile")"
+
+if [[ ! -f "$bfile" ]]; then
+	echo "Not a file [$bfile]"
+	exit 2
+fi
+
+# =======
+
+bbuild "$bfile"
+mkdir -p test/
+
+if [[ "$*" =~ --install ]]; then
+	sudo mv "$bname.tgz" "/usr/local/bin/$bname" && echo "Installed $bname to /usr/local/bin"
+else
+	mv "$bname".tgz "test/$bname"
+	echo "Stored "$bname" in test/$bname"
+fi
