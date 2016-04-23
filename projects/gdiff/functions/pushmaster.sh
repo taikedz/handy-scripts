76e45e3e198ff972b0ceb80aab42153502d51362
Tai @ Flex <dch.tai@gmail.com>
2016-04-23 15:03:55 +0100

    Use correct branch

--- projects/gdiff/functions/pushmaster.sh
+++ projects/gdiff/functions/pushmaster.sh
@@ -7,11 +7,15 @@
 ###
 
 function pushmaster {
+	local backbranch=$CURBRANCH
 	switchbranch master
-	git merge "$CURBRANCH"
+	git merge "$backbranch"
 	local origin=$(git remote -v|head -n 1|grep '(push)'|egrep -o '^[a-zA-Z0-9]+')
 	if [[ -n "$origin" ]] ; then
-		git push $origin "$CURBRANCH"
+		infoe "Pushing your changes on $backbranch to $origin"
+		git push $origin "$backbranch"
+		infoe "Pushing master to $origin"
+		git push $origin master
 	fi
-	switchbranch "$CURBRANCH"
+	switchbranch "$backbranch"
 }
