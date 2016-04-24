511d585c0798b538d9b49aa2cfe10ac1e588bb0d
Tai Kedzierski GBX <dch.tai@gmail.com>
2016-04-24 20:21:45 +0100

    secdir has its own project

---
+++ projects/secure-dir/files/config-init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#% include configreader
+export CONFFILE=.enc/config
+
+config_usesection Main
+export GREETING=$(config_readval welcome)
+
+
+
+config_usesection Mount
+
+export CWDALLOWED=yes
+if config_isset load_in_cwd; then CWDALLOWED=$(config_readval load_in_cwd); fi
+
+export LOADDIR=$PWD
+if config_isset home; then
+	LOADDIR=$(config_readval home)
+	LOADDIR=${LOADDIR//'$HOME'/$HOME}
+fi
