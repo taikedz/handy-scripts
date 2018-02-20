1661839ef0a5d56318af62a17d1ff9ef9afc34c6
Tai Kedzierski <dch.tai@gmail.com>
2018-02-20 16:45:55 +0000

    safer kernel cleanup

--- bin/rmkernel.sh
+++ bin/rmkernel.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
-# Versions are LABEL-VERSION-ISSUANCE-VARIANT
-#  we are interesetd in the VERSION-ISSUANCE pair
+# Script to remove extraneous kernels
 
-versions="$(ls /boot| grep -P "generic|vmlinuz" | sed -r -e 's/^[^-]+-([^-]+-[^-]+).+$/\1/g' | sort -V | uniq)"
+# If kernel updates or autoclean/autoremove fail because there is no more space in /boot
+#  then you can use this script to clean out old stuff and free up space to return to a normal state
 
-vcount=$(echo "$versions" | wc -l)
+# try
+#	./rmkernel.sh 2
+# this will print the removal commands
+# To apply the removals run
+#	./rmkernel.sh 2 | sudo bash
 
-[[ $# -gt 0 ]] || {
-	echo -e "You must specify the number of most recent images to keep.\n\nThere are currently $vcount kernel versions in /boot."
-	exit 1
+rmk:dpkg() {
+	dpkg --list 'linux-image*'|
+		grep ii|
+		awk '{print $3 "\t" $2}'|
+		sed -r 's/~\S+//'|
+		sort -V|
+		cut -f2|
+		head -n -"$1"| while read; do
+			echo dpkg --force-all --remove "$REPLY"
+		done
+	
+	apt-get autoclean && apt-get autoremove
 }
 
-keepcount="$1"; shift
+main() {
+	[[ $# -gt 0 ]] || {
+		echo -e "You must specify the number of most recent images to keep."
+		exit
+	}
 
-numpat='^[0-9]+$'
-[[ $keepcount =~ $numpat ]] || {
-	echo "[$keepcount] is not a number"
-	exit 2
+	matcher='^[0-9]+$'
+	[[ $1 =~ $matcher ]] || {
+		echo "$1 is not a number"
+		exit 2
+	}
+
+	echo "# Keeping the most recent $1 kernel(s)"
+	echo "# pipe to sudo bash to execute the following deletions:"
+
+	rmk:dpkg "$1"
 }
 
-echo -e "#\033[32;1m Keeping the newest $keepcount kernel(s)\033[0m"
-echo -e "#\033[33;1m pipe to sudo bash to execute the following deletions:\033[0m"
-
-# List versions, omit the last few (MINUS keepcount) which we are keeping
-delversions=($(echo "$versions" | head -n "-$keepcount" ))
-
-# The obverse
-keepversions=($(echo "$versions" | tail -n "$keepcount" ))
-
-echo ''
-
-echo "# Keeping:"
-for keepver in "${keepversions[@]}"; do
-	echo "# $keepver"
-done
-
-echo ''
-
-for ver in "${delversions[@]}"; do
-	ls /boot/*"$ver"* | sed -r -e 's/^/rm /g'
-done
-
-echo -e "\napt-get autoclean -y && apt-get autoremove -y\n"
-
+main "$@"
