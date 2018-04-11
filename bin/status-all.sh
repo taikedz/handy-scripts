f6df8895b0393e51bb53870da674d7b18b1fa24f
Tai Kedzierski <dch.tai@gmail.com>
2018-04-11 16:27:43 +0100

    Add help on status-all.sh

--- bin/status-all.sh
+++ bin/status-all.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+## Scour the current directory for git repositories
+## Report on the status
+## If not "working tree clean" in `git status output`
+##   then starts a new bash shell in that directory
+##
+## Once your are done in the repository, type
+##
+## exit 0 to continue to next repository
+##
+## exit 1 to stop processing repositories
+
 set -e
 
 # Rather than use bare binary, use all ASCII
@@ -7,6 +18,31 @@ set -e
 c_nul="$(echo -e "\033[0m")"
 c_red="$(echo -e "\033[31;1m")"
 c_yel="$(echo -e "\033[33;1m")"
+
+main() {
+	if [[ "$*" =~ --help ]]; then
+		egrep "^##" "$0"
+		exit 0
+	fi
+	
+	if [[ -n "${1:-}" ]]; then
+		git-verify "$1"
+	else
+		parts=(:)
+		while read target; do
+			parts[${#parts[@]}]="$target"
+		done < <(find . -name '.git')
+
+		for target in "${parts[@]:1}"; do
+			bash "$0" "$target" || die "$target"
+		done
+
+		echo "${c_yel}Processed"
+		for x in "${parts[@]:1}"; do echo "  $x"; done
+		echo "$c_nul"
+
+	fi
+}
 
 git-status() { (
 	cd "$1/.."
@@ -39,26 +75,6 @@ error() {
 die() {
 	error "FAIL: $*"
 	exit 1
-}
-
-main() {
-	if [[ -n "${1:-}" ]]; then
-		git-verify "$1"
-	else
-		parts=(:)
-		while read target; do
-			parts[${#parts[@]}]="$target"
-		done < <(find . -name '.git')
-
-		for target in "${parts[@]:1}"; do
-			bash "$0" "$target" || error "$target"
-		done
-
-		echo "${c_yel}Processed"
-		for x in "${parts[@]:1}"; do echo "  $x"; done
-		echo "$c_nul"
-
-	fi
 }
 
 main "$@"
