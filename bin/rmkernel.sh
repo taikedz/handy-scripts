aaf9348d4c1c8694f6645629427ca81e776fe9ff
Tai Kedzierski <dch.tai@gmail.com>
2018-12-13 12:57:05 +0000

    change tabs to spaces, add -y to autoclean/autoremove

--- bin/rmkernel.sh
+++ bin/rmkernel.sh
@@ -1,86 +1,91 @@
 #!/bin/bash
 
 # Script to remove extraneous kernels
-
+#
 # If kernel updates or autoclean/autoremove fail because there is no more space in /boot
 #  then you can use this script to clean out old stuff and free up space to return to a normal state
-
+#
 # try
-#	./rmkernel.sh 2
+#    ./rmkernel.sh 2
 # this will print the removal commands
+#
 # To apply the removals run
-#	./rmkernel.sh 2 | sudo bash
+#    ./rmkernel.sh 2 | sudo bash
 
 rmk:list-installed-kernel-info-pairs() {
-	dpkg --list 'linux-image*'|
-		grep ii|
-		awk '{print $3 "\t" $2}'|
-		sed -r 's/~\S+//'|
-		sort -V
+    dpkg --list 'linux-image*'|
+        grep ii|
+        awk '{print $3 "\t" $2}'|
+        sed -r 's/~\S+//'|
+        sort -V
 }
 
 rmk:list-installed-kernel-packages() {
-	rmk:list-installed-kernel-info-pairs|
-		cut -f2
+    rmk:list-installed-kernel-info-pairs|
+        cut -f2
 }
 
 rmk:list-installed-kernel-versions() {
-	rmk:list-installed-kernel-info-pairs|
-		cut -f1
+    rmk:list-installed-kernel-info-pairs|
+        cut -f1
 }
 
 rmk:clean-dpkg() {
-	# Perform a purge of package-less kernels
-	rmk:clean-boot
+    # Perform a purge of package-less kernels
+    rmk:clean-boot
 
-	echo ""
-	echo "# Keeping the most recent $1 kernel(s)"
-
-	rmk:list-installed-kernel-packages|
-		head -n -"$1"| while read; do
-			echo dpkg --force-all --remove "$REPLY"
-		done
-	
     echo ""
-	echo "apt-get autoclean && apt-get autoremove"
+    echo "# Keeping the most recent $1 kernel(s)"
 
-	echo "echo 'Fixing missing packages ...'"
-	echo "apt-get -f install"
+    rmk:list-installed-kernel-packages|
+        head -n -"$1"| while read; do
+            echo dpkg --force-all --remove "$REPLY"
+        done
+    
+    echo ""
+    echo "apt-get autoclean -y && apt-get autoremove -y"
+
+    echo "echo 'Fixing missing packages ...'"
+    echo "apt-get -f install -y"
 }
 
 rmk:shorten-version() {
-	# Remove the final number ("patch" (??) version)
-	
-	sed -r 's/.[0-9]+$//g'
+    # Remove the final number ("patch" (??) version)
+    #  since it is not used in file names in /boot
+    
+    sed -r 's/.[0-9]+$//g'
 }
 
 rmk:clean-boot() {
-	local current_versions="$(rmk:list-installed-kernel-versions|rmk:shorten-version|xargs echo|sed 's/ /|/g')"
+    # Previous attempts at kernel forced removal may have caused some
+    #  excess files to remain in /boot
+    # This clears such files out.
+    local current_versions="$(rmk:list-installed-kernel-versions|rmk:shorten-version|xargs echo|sed 's/ /|/g')"
 
-	echo "#  The following do not have a corresponding installed package:"
-	
-	ls /boot/config* /boot/initrd* /boot/retpoline* /boot/vmlinuz* /boot/abi* 2>/dev/null | grep -vP "$current_versions" | sed -r 's/^/rm /'
+    echo "#  The following do not have a corresponding installed package:"
+    
+    ls /boot/config* /boot/initrd* /boot/retpoline* /boot/vmlinuz* /boot/abi* 2>/dev/null | grep -vP "$current_versions" | sed -r 's/^/rm /'
 }
 
 main() {
-	[[ $# -gt 0 ]] || {
-		echo -e "You must specify the number of most recent images to keep."
-		exit
-	}
+    [[ $# -gt 0 ]] || {
+        echo -e "You must specify the number of most recent images to keep."
+        exit
+    }
 
-	echo "# Pipe the following output to \`sudo bash\` to execute the deletions:"
-	echo ""
+    echo "# Pipe the following output to \`sudo bash\` to execute the deletions:"
+    echo ""
 
-	if [[ "$1" =~ ^[0-9]+$ ]]; then
-		rmk:clean-dpkg "$1"
+    if [[ "$1" =~ ^[0-9]+$ ]]; then
+        rmk:clean-dpkg "$1"
 
-	elif [[ "$1" = clean-boot ]]; then
-		rmk:clean-boot
+    elif [[ "$1" = clean-boot ]]; then
+        rmk:clean-boot
 
-	else
-		echo "$1 is not a number"
-		exit 2
-	fi
+    else
+        echo "$1 is not a number"
+        exit 2
+    fi
 }
 
 main "$@"
