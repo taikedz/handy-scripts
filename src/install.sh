f3ec827f4abe360dc5f4ca7c71cadc4d6502071c
Tai Kedzierski <dch.tai@gmail.com>
2019-06-06 14:11:30 +0100

    Add config to installer

--- src/install.sh
+++ src/install.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$0")"
-
-mkdir -p ~/.local/bin
-
-if ! grep "PATH=" "$HOME/.bashrc" | grep '\.local/bin'; then
-    echo 'export PATH="$PATH:$HOME/.local/bin"' >> "$HOME/.bashrc"
-fi
-
-addscript() {
-    cp "$1" "$HOME/.local/bin/"
-}
-
-
 scripts_to_add=(
     bin/pstree
     bin/rmkernel.sh
@@ -31,6 +18,37 @@ scripts_to_add=(
     bin/z64
 )
 
+
+cd "$(dirname "$0")"
+
+mkdir -p ~/.local/bin
+
+if ! grep "PATH=" "$HOME/.bashrc" | grep '\.local/bin'; then
+    echo 'export PATH="$PATH:$HOME/.local/bin"' >> "$HOME/.bashrc"
+fi
+
+addscript() {
+    cp "$1" "$HOME/.local/bin/"
+}
+
+addconfig() {
+    if grep HANDYCONFIG "$2"; then
+        return 0
+    fi
+
+    if [[ "${SUDO:-}" = true ]]; then
+        cat "$1" | sudo tee -a "$2" >/dev/null
+    else
+        cat "$1" >> "$2"
+    fi
+
+    echo "Updated [$2]"
+}
+
 for script in "${scripts_to_add[@]}"; do
     addscript "$script"
 done
+
+addconfig configs/vimrc "$HOME/.vimrc"
+addconfig configs/user.bashrc "$HOME/.bashrc"
+SUDO=true addconfig configs/root.bashrc /etc/bash.bashrc
