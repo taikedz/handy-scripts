f0e8d963be5e7c735a2ef4de1340c78bd4e00dd5
Tai Kedzierski <dch.tai@gmail.com>
2026-02-05 15:27:33 +0000

    Fix new-go.sh

--- bin/new-go.sh
+++ bin/new-go.sh
@@ -3,7 +3,10 @@
 fail() { echo "$*"; exit 1; }
 
 progname="${1:-}"; shift || fail "Specify program binary name"
-modname="${1:-}" ; shift || fail "Specify mod name"
+modname="${1:-}" ; shift || fail "Specify mod name/URL"
+
+[[ "$progname" =~ ^[a-zA-Z0-9_-]+$ ]] || fail "Invalid program name '$progname"
+[[ "$modname" =~ ^[a-zA-Z0-9:./_-]+$ ]] || fail "Invalid mod name/URL '$modname"
 
 cat <<EOF >> "$progname.go"
 package main
@@ -33,18 +36,20 @@ EOF
 cat <<EOF >> build.sh
 #!/usr/bin/env bash
 
-HERE="$(dirname "$0")"
-cd "$HERE"
+HERE="\$(dirname "\$0")"
+cd "\$HERE"
 
 # Create a full-static binary that should not require dependencies on libc
 # https://stackoverflow.com/questions/61319677/flags-needed-to-create-static-binaries-in-golang
+#CGO_ENABLED=0 go build -trimpath -o bin/$progname -a -ldflags '-extldflags -static' "$progname.go"
 
-CGO_ENABLED=0 go build -trimpath -o bin/$progname -a -ldflags '-extldflags -static' "$progname.go"
+go build -trimpath -o "bin/$progname" "$progname.go"
 
 EOF
 
 chmod 755 build.sh
-(set -x
+
+echo "bin/" >> .gitignore
+
 go mod init "$modname"
-)
 
